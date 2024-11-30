89aaf96f48336c65e8f8e9e9c2508e596485ca88
Daniel <dannynebs@gmail.com>
2024-11-30 10:48:19 -0500

    Completed without stack

---
+++ DataStructures_Lab/DataStructures_Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please enter a word you would like to reverse");
+
+string userWord=Console.ReadLine();
+
+//Console.WriteLine(Reverse(userWord));
+
+
+
+
+
+
+
+//string Reverse(string input)
+//{
+//    char[] charArray = input.ToCharArray();
+//    Array.Reverse(charArray);
+//    return new string(charArray);
+//}
+
